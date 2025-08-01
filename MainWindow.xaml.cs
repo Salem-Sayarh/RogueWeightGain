91496f376328c3142c65558d0512d3566bb298db
Salem-Sayarh <s.sayarh.dev@outlook.com>
2025-08-02 01:39:59 +0200

    FIX NameSpace

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -10,7 +10,6 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using RWG.Pages;
 using RWG.Windows;
 
 namespace RWG
